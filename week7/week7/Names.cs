a16b07a1ca25ca5a238279f990102a9c406ecec0
ovidiuungureanu <117767402+ovidiuungureanu@users.noreply.github.com>
2023-01-15 22:44:46 +0200

    week7 homework

---
+++ week7/week7/Names.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week7
+{
+    public class Names
+    {
+        public void ReadAndCheckNames()
+        {
+            List<string> names = new List<string>();
+
+            Console.WriteLine("Enter Names, separated by a space:");
+
+            string input = Console.ReadLine();
+
+            string[] inputParsed = input.Split(" ");
+
+            names.AddRange(inputParsed);
+
+            try
+            {
+                CheckAllNames(names);
+                Console.WriteLine("All names are ok.");
+            }
+            catch(BlankNameException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+        private void CheckAllNames(List<string> names)
+        {
+            foreach(string name in names)
+            {
+                if (!IsValidName(name))
+                {
+                    throw new BlankNameException("The blank can't be a valid name");
+                }
+            }
+        }
+        private bool IsValidName(string name)
+        {
+            if (!name.Contains(""))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    public class BlankNameException : Exception
+    {
+        public BlankNameException(string? message) : base(message)
+        { }
+    }
+}
---
+++ week7/week7/Nums.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week7
+{
+    public class Nums
+    {
+        public void ReadAndCheckNumbers()
+        {
+            List<string> numbers = new List<string>();
+            Console.WriteLine("Enter Names, separated by a space:");
+            string input = Console.ReadLine();
+            string[] inputParsed = input.Split(" ");
+            numbers.AddRange(inputParsed);
+
+            void Convert(List<string> numbers)
+            {
+                List<int> newNums = new List<int>();
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    newNums.Add(int.Parse(numbers[i]));
+                }
+            }
+
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+
+        }
+        public int GetSum(List<string> numbers)
+        {
+            return numbers.Sum(str => Convert.ToInt32(str));
+        }
+    }
+
+    public class FormatException : Exception
+    {
+        public FormatException(string? message) : base(message)
+        { }
+    }
+
+    public class OverflowException : Exception
+    {
+        public OverflowException(string? message) : base(message)
+        { }
+    }
+
+    public class DivideByZeroException : Exception
+    {
+        public DivideByZeroException(string? message) : base(message)
+        { }
+    }
+}
